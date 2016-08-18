215037187c342c4e93b632dc4f3c7ab83ab32702
bearcheese <bearcheese@gmail.com>
2016-08-18 16:39:46 +0200

    MS-6 Moving to Maven

--- src/main/java/com/microservicesteam/nutaxi/eureka/EurekaServiceApplication.java
+++ src/main/java/com/microservicesteam/nutaxi/eureka/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.msteam;
+package com.microservicesteam.nutaxi.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
