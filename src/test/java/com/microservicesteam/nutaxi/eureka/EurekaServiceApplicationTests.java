e6a079d9852b4811e51d0926b1d111bbb7f22701
Balázs Szögi <szogibalu@gmail.com>
2016-11-30 20:04:29 +0100

    [MS-0] Simplified Docker setup

--- src/test/java/com/microservicesteam/nutaxi/eureka/EurekaServiceApplicationTests.java
+++ src/test/java/com/microservicesteam/nutaxi/eureka/EurekaServiceApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest
 public class EurekaServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
